3fce0a53651ddeccf5efc4355b24b60c756d2cdb
Fayyazuddin Syed <fayyazuddin@gmail.com>
2026-07-16 17:50:46 -0400

    Initial Commit

---
+++ Rogers-Marketplace AssignmentUITests/Rogers_Marketplace_AssignmentUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rogers_Marketplace_AssignmentUITestsLaunchTests.swift
+//  Rogers-Marketplace AssignmentUITests
+//
+//  Created by Fayyazuddin  Syed on 2026-07-16.
+//
+
+import XCTest
+
+final class Rogers_Marketplace_AssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
